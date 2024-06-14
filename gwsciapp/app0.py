8c2fffd38a07f4bc4c8f79a8938cfa88f3cf136d
Kennington <jameswkennington@gmail.com>
2024-06-14 13:48:51 -0400

    local imports

--- gwsciapp/app0.py
+++ gwsciapp/app0.py
@@ -6,7 +6,7 @@ import dash_bootstrap_components as dbc
 import dash_daq as daq
 from dash import dcc, Output, Input, html, State
 
-from gwsciapp import plot, waveforms
+import plot, waveforms
 
 # Configuration constants for app input boundaries
 MASS_MIN = 1.0
